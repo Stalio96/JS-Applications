d642be97a3f32a76e3fc8fc04bda16ae0ac42295
stalio <stalio_kirkov@abv.bg>
2022-03-17 17:42:22 +0200

    Added SPA

---
+++ 04.Single-Page-Applications/Lab/01.Calendar/app.js
@@ -0,0 +1,17 @@
+function years(){
+    const allYears = document.getElementById('years');
+
+    allYears.addEventListener('click', year);
+}
+
+function year(event){
+    if(event.target.textContent == 2020){
+        
+    }else if(event.target.textContent == 2021){
+
+    }else if(event.target.textContent == 2022){
+        
+    }else if(event.target.textContent == 2023){
+        
+    }
+}
